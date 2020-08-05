90220bab265dafc72922d3ba9b3631f19bfd00ce
Pranay <pranayburra66@gmail.com>
2020-08-05 07:32:52 +0530

    Updates

---
+++ precache-manifest.372c16365fc105763d3f7cb7813bd226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15362299f043908c7fbbc7d82dae036b",
+    "url": "/RecipeApi/index.html"
+  },
+  {
+    "revision": "fae317b522c472b39e7d",
+    "url": "/RecipeApi/static/css/main.9c338440.chunk.css"
+  },
+  {
+    "revision": "d25a7b4d54ecc5c5a54a",
+    "url": "/RecipeApi/static/js/2.d2e9e22c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApi/static/js/2.d2e9e22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae317b522c472b39e7d",
+    "url": "/RecipeApi/static/js/main.3d75cbc4.chunk.js"
+  },
+  {
+    "revision": "3c02a1e73d7608d9e060",
+    "url": "/RecipeApi/static/js/runtime-main.068115da.js"
+  }
+]);
